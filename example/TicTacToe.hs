44595ba1dc54896ad8b418f7df71ef384833ef01
Andrew Farmer <anfarmer@ku.edu>
2012-05-14 14:24:20 -0500

    TicTacToe example: server tracks moves and clears on a win.

--- example/TicTacToe.hs
+++ example/TicTacToe.hs
@@ -7,6 +7,7 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Default
+import Data.List (transpose)
 import Data.Monoid (mconcat)
 import qualified Data.Text.Lazy as T
 
@@ -29,21 +30,64 @@ web_app :: Document -> IO ()
 web_app doc = do
     register doc "click" "return { id : $(widget).attr('id') };"
 
-    turn <- newMVar "x.png"
+    turn <- newMVar X
+    board <- newMVar (replicate 9 Empty)
 
     forkIO $ forever $ do
         res <- waitFor doc "click"
-        let Success (Id i) :: Result Id = parse parseJSON res
+        let Success (Id i) :: Result Id = fromJSON res
         print i
 
-        img <- takeMVar turn
-        if img == "x.png" then putMVar turn "o.png" else putMVar turn "x.png"
-        send doc $ mconcat ["$('#", i, "').attr('src', '", img,"')"]
+        marker <- takeMVar turn
+        cb <- takeMVar board
+        let cb' = setPos i marker cb
+
+        case cb !! i of
+            Empty -> do putMVar board cb'
+                        if marker == X then putMVar turn O else putMVar turn X
+                        if winning cb'
+                        then do send doc $ mconcat ["$('.square').attr('src', '", imgFor Empty, "').addClass('click')"]
+                                swapMVar board (replicate 9 Empty)
+                                swapMVar turn X
+                                return ()
+                        else send doc $ mconcat ["$('#", T.pack (show i), "').attr('src', '", imgFor marker, "').removeClass('click')"]
+            _ -> return ()
+
 
     return ()
 
-data Id = Id T.Text deriving Show
+data Space = Empty | X | O deriving (Eq, Show)
+
+setPos :: Int -> Space -> [Space] -> [Space]
+setPos p m bd = b ++ (m:a)
+    where (b,a') = splitAt p bd
+          a = drop 1 a'
+
+imgFor :: Space -> T.Text
+imgFor Empty = "blank.png"
+imgFor X     = "x.png"
+imgFor O     = "o.png"
+
+winning :: [Space] -> Bool
+winning bd = or $ map winner $ rows bd ++ cols bd ++ diags bd
+
+winner :: [Space] -> Bool
+winner [X,X,X] = True
+winner [O,O,O] = True
+winner _       = False
+
+rows, cols, diags :: [Space] -> [[Space]]
+rows [] = []
+rows bd = take 3 bd : rows (drop 3 bd)
+
+cols = transpose . rows
+
+diags bd = [go bd,go (concatMap reverse (rows bd))]
+    where go [] = []
+          go (s:ss) = s : go (drop 3 ss)
+
+data Id = Id Int deriving Show
 
 instance FromJSON Id where
-   parseJSON (Object v) = Id App.<$> T.pack App.<$> (v .: "id")
+   parseJSON (Object v) = Id App.<$> read App.<$> (v .: "id")
    parseJSON _          = mzero
