52eb699dc911fe4cd4078254cb1d3ecf88d32ce5
Andrew Farmer <anfarmer@ku.edu>
2012-08-30 15:48:39 -0500

    Clean up example.

--- example/Main.hs
+++ example/Main.hs
@@ -1,40 +1,25 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, KindSignatures, GADTs, DoRec #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
 -- Example of using Kansas Comet
 
 module Main where
 
-import Data.Aeson as A
-import Data.Aeson.Types as AP
 import qualified Web.Scotty as Scotty
-import Web.Scotty (scottyOpts, get, file, literal, middleware)
+import Web.Scotty (scottyOpts, get, file, middleware)
 import Data.Default
-import Data.Map (Map)
 import Control.Monad
---import Control.Applicative
-import Control.Concurrent
-import Control.Concurrent.STM
 import Data.Monoid
-import Data.List as L
 import Control.Monad.IO.Class
 import Network.Wai.Middleware.Static
 import Data.Boolean
 
 -- import Network.Wai      -- TMP for debug
 
+import Web.KansasComet hiding ((:=))
 import qualified Web.KansasComet as KC
-import Web.KansasComet as KC ((<&>), event, send, registerEvents, waitForEvent, Document, prefix, kCometPlugin, connect, verbose)
 
-import Language.Sunroof
 import Language.Sunroof.Compiler
 import Language.Sunroof.Types
-
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text      as T
-
--- Async requests that something be done, without waiting for any reply
-async :: Document -> JS () -> IO ()
-async = undefined
 
 -- Sync requests that something be done, *and* waits for a reply.
 sync :: (Sunroof a) => Document -> JS a -> IO a
@@ -44,26 +29,22 @@ sync doc jsm = do
         send doc $ res
         return $ undefined
 
-
+main :: IO ()
 main = do
+    -- build the scotty dispatch app
+    scottyOpts (def { Scotty.verbose = 0 })  $ do
+        -- provide some static pages, include jquery
+        -- This is scotty code
 
+        let pol = only [("","index.html")]
+                  <|> ((hasPrefix "css/" <|> hasPrefix "js/") >-> addBase ".")
 
-        -- build the scotty dispatch app
-        scottyOpts (def { Scotty.verbose = 0 })  $ do
-                -- provide some static pages, include jquery
-                -- This is scotty code
+        middleware $ staticPolicy pol
 
-                let hasPrefix pre x = if pre`isPrefixOf` x then return x else Nothing
-
-                let policy =  only [("","index.html")]
-                          <|> ((hasPrefix "css/" <|> hasPrefix "js/") >-> addBase ".")
-
-                middleware $ staticPolicy $ policy
-
-                kcomet <- liftIO kCometPlugin
-                get "/js/kansas-comet.js" $ file $ kcomet
-                -- connect /example to the following web_app
-                connect opts web_app
+        kcomet <- liftIO kCometPlugin
+        get "/js/kansas-comet.js" $ file $ kcomet
+        -- connect /example to the following web_app
+        connect opts web_app
 
 opts :: KC.Options
 opts = def { prefix = "/example", verbose = 0 }
@@ -104,9 +85,6 @@ web_app doc = do
                     html :: JSString -> Action JSObject JSObject
                     html nm = method "html"  [cast nm]
 
---                    slider :: JSString -> JS (JFunction ())
---                    slider nm = call "$" with [cast nm]
-
                     fib :: JSNumber -> JS JSNumber
                     fib n = obj ! "fib" <$> with [cast n]
 
@@ -139,61 +117,6 @@ web_app doc = do
 
                 -- call control
                 control ()
-
-
-{-
-
-
-                        nm <- eval (event ! "eventname" :: JSString)
-                        ifB  (nm ==* "click")
-                             (do alert "CLICKy")
-                             (do alert "NO CLICKy")
-                        alert ("Finally")
---                        alert ("eventname = " <> (ifB (nm ==* "click") "CLICK" "NO CLICK"))
--}
-
-{-
-
---              foo <$> "c" <$> "d" := c
-
---                let v = c ! "pageX" :: JSNumber
-                v <- eval (c ! "pageX" :: JSNumber)
-                nm <- eval (c ! "eventname" :: JSString)
-                alert ("you clicked" <> cast v)
-                alert ("you clicked" <> cast c)
--}
-{-
-                switch  [ (nm .==. "click", ...)
-                        ]
--}
-
-
-{-
---        jsSelect $ JSS_Call "foo1" [cast (1 :: JSNumber)] :: JS ()
-
-        let control model = do
-                Just res <- waitForEvent doc (slide <> click)
-                case res of
-                  Slide _ n                      -> view n
-                  Click "up"    _ _ | model < 25 -> view (model + 1)
-                  Click "down"  _ _ | model > 0  -> view (model - 1)
-                  Click "reset" _ _              -> view 0
-                  _ -> control model
-
-            view model = do
-                let n = model
-                send doc $ concat
-                        [ "$('#slider').slider('value'," ++ show n ++ ");"
-                        , "$('#fib-out').html('fib " ++ show n ++ " = " ++ "&#171;&#8226;&#187;')"
-                        ]
-                -- sent a 2nd packet, because it will take time to compute fib
-                send doc ("$('#fib-out').text('fib " ++ show n ++ " = " ++ show (fib n) ++ "')")
-
-                control model
-
-
-        forkIO $ control 0
--}
         return ()
 
 fib n = if n < 2 then 1 else fib (n-1) + fib (n-2)
@@ -212,6 +135,4 @@ click = event "click" Click
             <&> "id"      ~= "$(widget).attr('id')"
             <&> "pageX"   ~=  "event.pageX"
             <&> "pageY"   ~=  "event.pageY"
-
-events = slide <> click
 
