3075b46028683829cb3b5e3abd10e4608569ca10
Andrew Farmer <anfarmer@ku.edu>
2012-09-06 13:40:30 -0500

    Clean up example.

--- example/Main.hs
+++ example/Main.hs
@@ -1,11 +1,8 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
--- Example of using Kansas Comet
-
 module Main where
 
-import qualified Web.Scotty as Scotty
-import Web.Scotty (scottyOpts, get, file, middleware)
+import Web.Scotty (scotty, middleware)
 import Data.Default
 import Control.Monad
 import Data.Monoid
@@ -13,34 +10,23 @@ import Control.Monad.IO.Class
 import Network.Wai.Middleware.Static
 import Data.Boolean
 
--- import Network.Wai      -- TMP for debug
-
-import Web.KansasComet hiding ((:=))
+import Web.KansasComet
 import qualified Web.KansasComet as KC
 
 import Language.Sunroof
-import Language.Sunroof.Compiler
 import Language.Sunroof.Types
 
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text      as T
-
 main :: IO ()
-main = do
-    -- build the scotty dispatch app
-    scottyOpts (def { Scotty.verbose = 0 })  $ do
-        -- provide some static pages, include jquery
-        -- This is scotty code
+main = scotty 3000 $ do
+    kcomet <- liftIO kCometPlugin
 
-        let pol = only [("","index.html")]
-                  <|> ((hasPrefix "css/" <|> hasPrefix "js/") >-> addBase ".")
+    let pol = only [("","index.html")
+                   ,("js/kansas-comet.js", kcomet)]
+              <|> ((hasPrefix "css/" <|> hasPrefix "js/") >-> addBase ".")
 
-        middleware $ staticPolicy pol
+    middleware $ staticPolicy pol
 
-        kcomet <- liftIO kCometPlugin
-        get "/js/kansas-comet.js" $ file $ kcomet
-        -- connect /example to the following web_app
-        connect opts web_app
+    KC.connect opts web_app
 
 opts :: KC.Options
 opts = def { prefix = "/example", verbose = 0 }
@@ -50,70 +36,72 @@ default(JSNumber, JSString, String)
 -- This is run each time the page is first accessed
 web_app :: Document -> IO ()
 web_app doc = do
-        print "web_app"
+    registerEvents doc "body" (slide <> click)
 
-        registerEvents doc "body" (slide <> click)
+    sync doc $ do
+        obj <- new
+        obj <$> "model" := (0 :: JSNumber)
 
-        sync doc $ do
-                obj <- new
-                obj <$> "model" := (0 :: JSNumber)
+        -- This is using the imperative update to enable the
+        let control :: () -> JS ()
+            control () = obj ! "control" <$> with []
 
-                -- This is using the imperative update to enable the
-                let control :: () -> JS ()
-                    control () = obj ! "control" <$> with []
+            view :: () -> JS ()
+            view () = obj ! "view" <$> with []
 
-                    view :: () -> JS ()
-                    view () = obj ! "view" <$> with []
+            addMethod :: (Sunroof a, Sunroof b) => String -> (a -> JS b) -> JS ()
+            addMethod nm f = do n <- function f
+                                obj <$> nm := n
 
-                    set :: (Sunroof a, Sunroof b) => String -> (a -> JS b) -> JS ()
-                    set nm f = do n <- function f
-                                  obj <$> nm := n
+            jQuery :: JSString -> JS JSObject
+            jQuery nm = call "$" <$> with [cast nm]
 
-                    eventname :: JSSelector JSString
-                    eventname = label "eventname"
+            slider :: JSValue -> Action JSObject JSObject
+            slider nm = method "slider"  ["value", cast nm]
 
-                    jQuery :: JSString -> JS JSObject
-                    jQuery nm = call "$" <$> with [cast nm]
+            html :: JSString -> Action JSObject JSObject
+            html nm = method "html"  [cast nm]
 
-                    slider :: JSValue -> Action JSObject JSObject
-                    slider nm = method "slider"  ["value", cast nm]
+            fib :: JSNumber -> JS JSNumber
+            fib n = obj ! "fib" <$> with [cast n]
 
-                    html :: JSString -> Action JSObject JSObject
-                    html nm = method "html"  [cast nm]
+            update :: String -> JSNumber -> JSNumber -> JSNumber -> JS ()
+            update nm val mn mx =
+                ifB ((val <=* mx) &&* (val >=* mn))
+                    (obj <$> nm := val)
+                    (return ())
 
-                    fib :: JSNumber -> JS JSNumber
-                    fib n = obj ! "fib" <$> with [cast n]
+            switchB _   []         def = def
+            switchB tag ((a,b):xs) def = ifB (tag ==* a) b (switchB tag xs def)
 
-                set "fib" $ \ (n :: JSNumber) ->
-                        ifB (n <* 2)
-                                (return 1)
-                                (liftM2 (+) (fib (n - 1)) (fib (n - 2)))
+        addMethod "fib" $ \ (n :: JSNumber) ->
+            ifB (n <* 2)
+                (return 1)
+                (liftM2 (+) (fib (n - 1)) (fib (n - 2)))
 
-                set "control" $ \ () -> wait "body" (slide <> click) $ \ event -> do
-                        model <- eval (obj ! "model") :: JS JSNumber
-                        model' <- ifB ((event ! eventname) ==* "slide")
-                                        (return (event ! "value" :: JSNumber))
-                                $ ifB ((event ! "id" :: JSString) ==* "up" &&* model <* 25)
-                                        (return (model + 1))
-                                $ ifB ((event ! "id" :: JSString) ==* "down" &&* model >* 0)
-                                        (return (model - 1))
-                                $ ifB ((event ! "id" :: JSString) ==* "reset")
-                                        (return 0)
-                                $ (return model)
-                        obj <$> "model" := model'
-                        view ()
+        addMethod "control" $ \ () ->
+            wait "body" (slide <> click) $ \ res -> do
+                model <- eval (obj ! "model") :: JS JSNumber
 
-                set "view" $ \ () -> do
-                        model <- eval (obj ! "model") :: JS JSNumber
-                        jQuery "#slider"  <*> slider (cast model)
-                        jQuery "#fib-out" <*> html ("fib " <> cast model <> "...")
-                        res <- fib model
-                        jQuery "#fib-out" <*> html ("fib " <> cast model <> " = " <> cast res)
-                        control ()
+                switchB (res ! "id" :: JSString)
+                    [ ("slider", update "model" (res ! "value") 0 25)
+                    , ("up"    , update "model" (model + 1)     0 25)
+                    , ("down"  , update "model" (model - 1)     0 25)
+                    , ("reset" , update "model" 0               0 25)
+                    ] $ return ()
 
-                -- call control
-                control ()
-        return ()
+                view ()
+
+        addMethod "view" $ \ () -> do
+            model <- eval (obj ! "model") :: JS JSNumber
+            jQuery "#slider"  <*> slider (cast model)
+            jQuery "#fib-out" <*> html ("fib " <> cast model <> "...")
+            res <- fib model
+            jQuery "#fib-out" <*> html ("fib " <> cast model <> " = " <> cast res)
+            control ()
+
+        control ()
+    return ()
 
 fib n = if n < 2 then 1 else fib (n-1) + fib (n-2)
 
@@ -121,14 +109,12 @@ data Event = Slide String Int
            | Click String Int Int
     deriving (Show)
 
-(~=) = (KC.:=)
-
 slide = event "slide" Slide
-            <&> "id"      ~= "$(widget).attr('id')"
-            <&> "value"   ~= "aux.value"
+            <&> "id"      .= "$(widget).attr('id')"
+            <&> "value"   .= "aux.value"
 
 click = event "click" Click
-            <&> "id"      ~= "$(widget).attr('id')"
-            <&> "pageX"   ~=  "event.pageX"
-            <&> "pageY"   ~=  "event.pageY"
+            <&> "id"      .= "$(widget).attr('id')"
+            <&> "pageX"   .=  "event.pageX"
+            <&> "pageY"   .=  "event.pageY"
 
