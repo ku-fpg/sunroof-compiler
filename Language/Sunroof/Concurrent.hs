f3a261c0910be87f4dd2815687145f25c2cc8094
Andy Gill <andygill@ku.edu>
2013-03-03 18:15:02 -0600

    Starting writing writeChan.

--- Language/Sunroof/Concurrent.hs
+++ Language/Sunroof/Concurrent.hs
@@ -2,9 +2,13 @@
 
 module Language.Sunroof.Concurrent where
 
+import Data.Boolean
+
 import Language.Sunroof.Types
 import Language.Sunroof.Types (T(A,B))
-import Language.Sunroof.JS.Browser(window,setTimeout)
+import Language.Sunroof.JS.Browser(window,setTimeout,alert)
+
+
 
 loopJS :: JSB () -> JSB ()      -- does not terminate
 loopJS m = do
@@ -37,3 +41,55 @@ yieldJSB = threadDelayJSB 0
 -- break out of the JSB
 stopJSB :: JSB ()
 stopJSB = reifyccJS $ \ _ -> return ()
+
+data JSChan a = JSChan (JSArray (JSFunction a ())) (JSArray ())
+
+
+writeChan :: (JSThread t, JSArgument a) => JSChan a -> a -> JS t ()
+writeChan (JSChan recvs sends) a = do
+        ifB (lengthArray recvs ==* 0)
+            (do alert ("no one listening")
+            )
+            (do f <- popArray recvs
+                apply f a       -- actually runs this thread for a bit.
+                                -- remember, it can block, but the underlying
+                                -- javascript will yield.
+                                -- not sure if this could cause race conditions?
+                                -- There *is* no blocking, so I think not.
+                return ()
+            )
+
+
+{-
+- reading a channel can block
+readChan :: JSChan a -> ThreadedJS a
+readChan (JSChan obj) = ThreadedJS $ \ k -> do
+        --- first, figure out if there is anything in the queue
+        sends :: JSArray JSObject <- evaluate $ obj ! "sends"
+        recvs :: JSArray JSObject <- evaluate $ obj ! "recvs"
+        ifB (lengthJS sends ==* 0)
+            (do -- if empty, add youself to a callback queue
+                -- push(
+                return ()
+            )
+            (do -- if not empty, take the next thing off the channel,
+                -- and return it (using the continuation)
+--                fn <- recvs.pop()
+--                apply
+                return ()
+            )
+
+--        pop(obj ! "recvsn")
+
+-}
+
+
+
+
+
+
+
+
+
+
+
