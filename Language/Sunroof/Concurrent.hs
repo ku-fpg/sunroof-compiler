93103f282f5fd99f5227e677314bf883ad47ba41
Andy Gill <andygill@ku.edu>
2013-03-05 23:37:52 -0600

    Removing old code.

--- Language/Sunroof/Concurrent.hs
+++ Language/Sunroof/Concurrent.hs
@@ -77,26 +77,6 @@ readChan (JSChan written waiting) = do
 
 
 {-
-- reading a channel can block
-
-readChan (JSChan obj) = ThreadedJS $ \ k -> do
-        --- first, figure out if there is anything in the queue
-        sends :: JSArray JSObject <- evaluate $ obj ! "sends"
-        recvs :: JSArray JSObject <- evaluate $ obj ! "recvs"
-        ifB (lengthJS sends ==* 0)
-            (do -- if empty, add youself to a callback queue
-                -- push(
-                return ()
-            )
-            (do -- if not empty, take the next thing off the channel,
-                -- and return it (using the continuation)
---                fn <- recvs.pop()
---                apply
-                return ()
-            )
-
---        pop(obj ! "recvsn")
-
 -}
 
 
