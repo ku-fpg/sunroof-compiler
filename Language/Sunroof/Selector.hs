81e5a0b3030ca8c75cb0c8f94d96c170098354e8
Jan Bracker <jan.bracker@googlemail.com>
2013-03-10 19:02:45 -0500

    Documentation for 'Selector'.

--- Language/Sunroof/Selector.hs
+++ Language/Sunroof/Selector.hs
@@ -17,25 +17,40 @@ import Language.Sunroof.JS.String ( JSString, string )
 import Language.Sunroof.JS.Number ( JSNumber )
 import Language.Sunroof.JS.Object ( JSObject )
 
--- | a 'JSSelector' selects a field from a JSObject.
--- The phantom is the type of the selected value.
-{-data JSSelector :: * -> * where
-        JSSelector :: JSString           -> JSSelector a-}
+-- | A 'JSSelector' selects a field or attribute from a Javascript object.
+--   The phantom is the type of the selected value. Note the selected 
+--   field or attributes may also array entries ('index').
 data JSSelector a = JSSelector Expr
 
+-- | Selectors can be created from the name of their attribute.
 instance IsString (JSSelector a) where
   fromString = JSSelector . unbox . string
 
 instance Show (JSSelector a) where
   show (JSSelector ids) = show ids
 
-
+-- | Create a selector for a named field or attribute.
+--   For type safty it is adivsed to use this with an 
+--   accompanying type signature. Example:
+--   
+-- > array ! label "length"
+--   
+--   See '!' for further information on usage.
 label :: JSString -> JSSelector a
 label = JSSelector . unbox
 
+-- | Create a selector for an indexed value (e.g. array access).
+--   For type safty it is adivsed to use this with an 
+--   accompanying type signature. Example:
+--   
+-- > array ! index 4
+--   
+--   See '!' for further information on usage.
 index :: JSNumber -> JSSelector a
 index = JSSelector . unbox
 
+-- | Provided for internal usage by the compiler. Unwraps the 
+--   selector.
 unboxSelector :: JSSelector a -> Expr
 unboxSelector (JSSelector e) = e
 
@@ -43,6 +58,10 @@ unboxSelector (JSSelector e) = e
 
 infixl 1 !
 
+-- | Operator to use a selector on a Javascript object. Examples:
+--   
+-- > array ! label "length"
+-- > array ! index 4
 (!) :: forall o a . (Sunroof o, Sunroof a) => o -> JSSelector a -> a
 (!) arr (JSSelector idx) = box $ Dot (ExprE $ unbox arr) 
                                      (ExprE $ idx) 
