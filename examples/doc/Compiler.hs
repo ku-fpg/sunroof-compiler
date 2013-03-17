d8dc8835cc18dc848a6ad60bb8b118a677081739
Andy Gill <andygill@ku.edu>
2013-03-17 14:54:14 -0500

    Adding the second haddock example.

--- examples/doc/Compiler.hs
+++ examples/doc/Compiler.hs
@@ -7,3 +7,7 @@ main = do
      	    alert(js "Hello");
      putStrLn txt
 
+     txt <- sunroofCompileJS def "main" $ do
+            function $ \ n -> do
+                return (n * (n :: JSNumber))
+     putStrLn txt
