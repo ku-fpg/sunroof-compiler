262535f1cdf3fd1ae7c03525afa95773afaf2225
Andy Gill <andygill@ku.edu>
2013-04-13 13:57:23 -0500

    Updating map to use the Functor API.

--- Language/Sunroof/JS/Map.hs
+++ Language/Sunroof/JS/Map.hs
@@ -1,5 +1,6 @@
 
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- | 'JSMap' provides an abstract and more type-safe access to maps
 --   in JavaScript. It is a wrapper around the dictionary each object
@@ -8,19 +9,26 @@ module Language.Sunroof.JS.Map
   ( JSMap
   , newMap
   , insert
-  , lookup'
+  , lookup
+  , lookup'     -- TO remove in next major version change
+  , delete
   , size
+  , elems
   ) where
 
 import Data.Boolean ( IfB(..), BooleanOf )
 
+import Prelude hiding ( lookup )
 import Language.Sunroof.Classes
   ( Sunroof(..) )
-import Language.Sunroof.Types
-import Language.Sunroof.Selector ( (!) )
+import Language.Sunroof.Types hiding ( delete )
+import qualified Language.Sunroof.Types as T
+import Language.Sunroof.Selector ( JSSelector, (!), label )
 import Language.Sunroof.JS.Object ( JSObject )
 import Language.Sunroof.JS.Bool ( JSBool, jsIfB )
 import Language.Sunroof.JS.Number
+import Language.Sunroof.JS.String
+import qualified Language.Sunroof.JS.Array as A
 
 -- -------------------------------------------------------------
 -- JSMap Type
@@ -53,21 +61,52 @@ newMap = do
   o <- new "Object" ()
   return $ JSMap o
 
+
 -- | @insert k x@ inserts an element @x@ associated with the given
 --   key @k@ into a map.
 insert :: (SunroofKey k, Sunroof a) => k -> a -> JSMap k a -> JS t ()
 insert k a (JSMap o) = do
         o # jsKey k := a
 
--- | @lookup k@ selects the value associated with the key @k@.
+{-# DEPRECATED lookup' "Don't use lookup'; use lookup instead" #-}
 lookup' :: (SunroofKey k, Sunroof a) => k -> JSMap k a -> JS t a
-lookup' k (JSMap o) = do
+lookup' = lookup
+
+-- | @lookup k@ selects the value associated with the key @k@.
+lookup :: (SunroofKey k, Sunroof a) => k -> JSMap k a -> JS t a
+lookup k (JSMap o) = do
         evaluate $ o ! jsKey k
 
 size :: JSMap k a -> JS t JSNumber
 size (JSMap o) = fun "Object.size" $$ o
 
---deleteMap :: (SunroofKey k, Sunroof a) => k -> JSMap k a -> JS t ()
---deleteMap k (JSMap o) = fun "delete" $$ (o ! jsKey k)
+delete :: (SunroofKey k, Sunroof a) => k -> JSMap k a -> JS t ()
+delete k (JSMap o) = o # T.delete (jsKey k :: JSSelector ())
 
+-- | get the elements in a @Map@. If you want the keys, then keep
+-- a second @Map@ that maps to the keys, or store the key in the target element.
+elems :: forall k a t . (SunroofKey k, Sunroof a) => JSMap k a -> JS t (A.JSArray a)
+elems mp = do
+        a0 <- selectors mp
+        a0 # jsMap (\ s -> return $ mp ! s)
 
+selectors :: (SunroofKey k, Sunroof a) => JSMap k a -> JS t (A.JSArray (JSSelector a))
+selectors (JSMap o) = fun "Object.keys" $$ o
+
+instance SunroofKey k => SunroofFunctor (JSMap k) where
+  forEach body mp = do
+        arr <- mp # elems
+        arr # forEach body
+        return ()
+{-
+  -- TODO
+  jsMap body mp@(JSMap o) = do
+        mp1@(JSMap o1) <- newMap
+        arr <- mp # elems
+        arr # forEach (\ k -> do
+                v <- evaluate $ o ! k
+                v1 <- body v
+                v
+
+        return
+-}
